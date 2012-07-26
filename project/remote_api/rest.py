c3141a85bc29efb187cee3c6a8db7bd492a7e609
Klemens Mantzos <klemens@fetzig.at>
2012-07-26 17:31:20 +0200

    added classes to connect to datasource/datasink part of rest api

--- project/remote_api/rest.py
+++ project/remote_api/rest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import json
 import requests
 
 REST_API_BASE_URL = "http://localhost:8080/"
@@ -8,6 +9,9 @@ REST_API_BASE_URL = "http://localhost:8080/"
 # move all requests api specific code to RestBase
 
 class RestBase(object):
+    u'''
+    To abstract the requests api
+    '''
     
     def __init__(self, url=REST_API_BASE_URL):
         self.base_url = url
@@ -17,8 +21,13 @@ class RestBase(object):
         return req
     
     def _get(self, path="", data=None):
-        req = requests.get(self.base_url + path, data=data)
-        return req
+        response = requests.get(self.base_url + path, data=data)
+        if response.status_code == 404:
+            return False
+        elif response.status_code == 204:
+            return True
+        else:
+            return response.json
     
     def _post(self, path="", data=None):
         req = requests.post(self.base_url + path, data=data)
@@ -30,8 +39,11 @@ class RestBase(object):
 
 
 class RestUser(RestBase):
+    '''
+    crud + "check login" methods for user accounts
+    '''
     
-    def __init__(self, username=None, path="users/"):
+    def __init__(self, username, path="users/"):
         if not username:
             raise ValueError('argument "username" is missing.')
         self.username = username
@@ -40,16 +52,21 @@ class RestUser(RestBase):
         self.base_url = "%s%s%s/" % (self.base_url, path, self.username)
     
     def delete(self):
+        '''
+        delete user
+        '''
         return self._delete()
     
     def get(self):
-        response = self._get()
-        if response.status_code == 404:
-            return False
-        else:
-            return response.json
+        '''
+        get user data or check if user exists
+        '''
+        return self._get()
         
     def post(self, data):
+        '''
+        create/register/signup user
+        '''
         if not data:
             raise ValueError('argument "data" is missing.')
         # validate?
@@ -61,6 +78,9 @@ class RestUser(RestBase):
         return self._post('register', req_params)
     
     def put(self, data):
+        '''
+        change user data
+        '''
         if not data:
             raise ValueError('argument "data" is missing.')
         # validate?
@@ -79,7 +99,12 @@ class RestUser(RestBase):
         
         return self._put(data=req_params)
     
-    def login_sic(self, password):
+    def check_login(self, password):
+        '''
+        checks if user-password combination is valid to login.
+        
+        the actual login and session handling is done by the backmeup-frontend.
+        '''
         if not username:
             raise ValueError('argument "username" is missing.')
         if not password:
@@ -89,9 +114,136 @@ class RestUser(RestBase):
             'password': password,
         }
         
-        response = self._get('/login', req_params)
-        if response.status_code == 204:
-            return True
-        else:
-            return False
+        return = self._get('/login', req_params)
+        
 
+class RestDatasource(RestBase):
+    def __init__(self, path="datasources/"):
+        super(RestDatasource, self).__init__()
+        self.base_url = self.base_url + path
+    
+    def get_all(self):
+        return self._get()
+    
+    def post(self, data):
+        return self._post(data=data)
+    
+    def delete(self, datasource_id):
+        return self._delete(path=datasource_id)
+    
+
+
+class RestDatasourceProfile(RestBase):
+    
+    def __init__(self, username, path="datasources/"):
+        if not username:
+            raise ValueError('argument "username" is missing.')
+        self.username = username
+        
+        super(RestDatasourceProfile, self).__init__()
+        self.base_url = "%s%s%s/" % (self.base_url, path, self.username)
+    
+    def get_all(self):
+        return self._get(path="profiles/")
+    
+    def delete(self, profile_id):
+        if not profile_id:
+            raise ValueError('argument "profile_id" is missing.')
+        return self._delete(path="profiles/" + profile_id)
+    
+    def options(self, profile_id):
+        if not profile_id:
+            raise ValueError('argument "profile_id" is missing.')
+        
+        return self._get(path="profiles/" + profile_id + "/options")
+        
+    def put(self, profile_id, source_options):
+        if not profile_id:
+            raise ValueError('argument "profile_id" is missing.')
+        if not source_options:
+            raise ValueError('argument "source_options" is missing.')
+        
+        data = {
+            "sourceOptions": source_options,
+        }
+        return self._put(path="profiles/" + profile_id, data=data)
+    
+    def auth(self, datasource_id, data):
+        if not "profile_name" in data:
+            raise ValueError('key "profile_name is missing in argument "data".')
+        if not "key_ring" in data:
+            raise ValueError('key "key_ring is missing in argument "data".')
+        params = {
+            "profileName": data["profile_name"],
+            "keyRing": data["key_ring"],
+        }
+        return self._post(path="%s/auth" % datasource_id, data=params)
+    
+    
+    def auth_post(self, profile_id, data):
+        if not "profile_name" in data:
+            raise ValueError('key "profile_name is missing in argument "data".')
+        if not "key_ring" in data:
+            raise ValueError('key "key_ring is missing in argument "data".')
+        params = {
+            "keyRing": data["key_ring"],
+            "sourceOptions": json.dumps(data['source_options']),
+        }
+        return self._post(path="%s/auth/post" % profile_id, data=params)
+
+
+class RestDatasink(RestBase):
+    
+    def __init__(self, path='datasinks/'):
+        super(RestDatasink, self).__init__()
+        self.base_url = self.base_url + path
+    
+    def get_all(self):
+        return self._get()
+    
+    def post(self, data):
+        return self._post(data)
+    
+    def delete(self, datasink_id):
+        return self._delete(path=datasink_id)
+    
+
+class RestDataskinProfile(RestBase):
+    
+    def __init__(self, username, path="datasinks/"):
+        if not username:
+            raise ValueError('argument "username" is missing.')
+        self.username = username
+        
+        super(RestDatasourceProfile, self).__init__()
+        self.base_url = "%s%s%s/" % (self.base_url, path, self.username)
+    
+    def get_all(self):
+        return self._get(path="profiles/")
+    
+    def delete(self, profile_id):
+        return self._delete(path="profiles/" + profile_id)
+    
+    def auth(self, datasink_id, data):
+        if not "profile_name" in data:
+            raise ValueError('key "profile_name is missing in argument "data".')
+        if not "key_ring" in data:
+            raise ValueError('key "key_ring is missing in argument "data".')
+        params = {
+            "profileName": data["profile_name"],
+            "keyRing": data["key_ring"],
+        }
+        return self._post(path="%s/auth" % datasink_id, data=params)
+    
+    def auth_post(self, profile_id, data):
+        if not "profile_name" in data:
+            raise ValueError('key "profile_name is missing in argument "data".')
+        #if not "key_ring" in data:
+        #    raise ValueError('key "key_ring is missing in argument "data".')
+        params = {
+            "keyRing": data["key_ring"],
+            #"sourceOptions": json.dumps(data['source_options']),
+        }
+        return self._post(path="%s/auth/post" % profile_id, data=params)
+    
+        
