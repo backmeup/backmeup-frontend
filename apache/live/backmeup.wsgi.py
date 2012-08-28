255fff23414ca85c2c32b2f66661cf83df68e5fb
Klemens Mantzos <klemens@fetzig.at>
2012-08-28 14:34:41 +0200

    updated wsgi settings

--- apache/live/backmeup.wsgi.py
+++ apache/live/backmeup.wsgi.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 # same as settings.BASE_ROOT and settings.PROJECT_ROOT
-BASE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+BASE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 PROJECT_ROOT = os.path.join(BASE_ROOT, 'project')
 
 sys.path = [
@@ -15,7 +15,7 @@ sys.path = [
 # add site packages from viratualenv.
 # NOTE: maybe obsolet
 import site
-site.addsitedir('/data/backmeup-frontend-env/lib/python2.6/site-packages')
+site.addsitedir('/data/backmeup-frontend-env/lib/python2.7/site-packages')
 
 # restart wsgi process if source file are changed
 #import monitor
