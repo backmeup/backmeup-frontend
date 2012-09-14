8d8c3adf45973b8c2cfafa04b94e0c85c0193484
Klemens Mantzos <klemens@fetzig.at>
2012-09-14 15:37:25 +0200

    polished signup form

--- project/access/forms.py
+++ project/access/forms.py
@@ -13,6 +13,78 @@ class UserCreationForm(forms.ModelForm):
     """
     A form that creates a user, with no privileges, from the given username and password.
     """
+    #username = forms.RegexField(label=_("Username"), max_length=30, regex=r'^[\w.@+-]+$',
+    #    help_text=_("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
+    #    error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+    email = forms.EmailField(label=_('Email'), max_length=254)
+
+    password1 = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"), widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+
+    #key_ring1 = forms.CharField(label=_("KeyRing Password"), widget=forms.PasswordInput)
+    #key_ring2 = forms.CharField(label=_("KeyRing Password confirmation"), widget=forms.PasswordInput,
+    #    help_text=_("Enter the same KeyRing Password as above, for verification."))
+
+    class Meta:
+        model = User
+        fields = ("email")
+
+    def clean_username(self):
+        return self.cleaned_data["username"]
+    
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        try:
+            # check if remote api knows user
+            remote_user = RestUser(email)
+            result = remote_user.get()
+        
+            if 'errorType' in result and result['errorType'] == 'org.backmeup.model.exceptions.UnknownUserException':
+                # remote api doesn't know user, thus check with local db
+                User.objects.get(username=email)
+        except User.DoesNotExist:
+            return email
+        
+        raise forms.ValidationError(_("A user with that email already exists."))
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1", "")
+        password2 = self.cleaned_data["password2"]
+        if password1 != password2:
+            raise forms.ValidationError(_("The two password fields didn't match."))
+        return password2
+
+    def clean(self):
+        if not self.errors:
+            self.cleaned_data['username'] = self.cleaned_data['email']
+        super(UserCreationForm, self).clean()
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        #user.set_password(self.cleaned_data["password1"])
+        user.set_password("")
+
+        remote_user = RestUser(user.username)
+        response = remote_user.post({
+            'password': self.cleaned_data["password1"],
+            'keyRing': self.cleaned_data['password1'],
+            'email': self.cleaned_data['email'],
+        })
+
+        if response == False:
+            raise Exception
+
+        if commit:
+            user.save()
+        return user
+
+
+class DebugUserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and password.
+    """
     username = forms.RegexField(label=_("Username"), max_length=30, regex=r'^[\w.@+-]+$',
         help_text=_("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
         error_messages={'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
@@ -60,7 +132,7 @@ class UserCreationForm(forms.ModelForm):
         return key_ring2
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
+        user = super(DebugUserCreationForm, self).save(commit=False)
         #user.set_password(self.cleaned_data["password1"])
         user.set_password("")
 
