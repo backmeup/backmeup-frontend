e5ed77f7a9ac005ada84b1c9409b1740fcb7b673
Klemens Mantzos <klemens@fetzig.at>
2012-10-02 17:51:01 +0200

    fixed name error

--- project/main/forms.py
+++ project/main/forms.py
@@ -72,7 +72,7 @@ class DatasourceAuthForm(forms.Form):
 
                 self.fields['input_value_%s' % i] = forms.CharField(**field_kwargs)
         # add "profile options" form field(s)
-        rest_datasource_profile = RestDatasourceProfile(username=username)
+        rest_datasource_profile = RestDatasourceProfile(username=self.username)
         print "#####################asdf", rest_datasource_profile.options(profile_id=self.auth_data['profileId'])
         
     def rest_save(self, username, key_ring):
